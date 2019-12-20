39f1c3a7c08adf1745c0225a5b2d5edaab984000
Miwha Geschwind <miwhag@gmail.com>
2019-12-20 03:52:53 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,6 +3,28 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+def introduction(name) 
+  puts "Hi, my name is #{name}."
+end
+
+introduction("miwha")
+
+
+
+def introduction_with_language(name, language)
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end 
+
+introduction_with_language("bob,ruby")
+
+
+def introduction_with_language_optional(name, language = "ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("jack")
+
+
 
 
 
